abb81d3d6d3e055cb77aa33908083dac8902b554
Daiana <daianalimaep@gmail.com>
2023-11-17 12:19:33 -0300

    fix: mudando nodemon para node e subir para produção

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,29 @@
+const knex = require("knex");
 require("dotenv").config();
 
-const knex = require("knex")({
-  client: "sqlite3",
-  connection: {
-    filename: "data/portal.sqlite",
-  },
-  useNullAsDefault: true,
-});
+let connectionConfig;
 
-module.exports = knex;
+if (process.env.ENV === "production") {
+  connectionConfig = {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: { rejectUnauthorized: false },
+    },
+  };
+} else {
+  connectionConfig = {
+    client: "sqlite3",
+    connection: {
+      filename: "data/portal.sqlite",
+    },
+    useNullAsDefault: true,
+  };
+}
+
+const db = knex(connectionConfig);
+module.exports = db;
