0b8a05d6bb2db777c53ef8aab43e937d91c934a7
Daiana <daianalimaep@gmail.com>
2023-11-17 13:26:58 -0300

    Refactor: ajustes

--- scripts/migration.js
+++ scripts/migration.js
@@ -1,16 +1,15 @@
 const knex = require("knex");
 const fs = require("fs");
 
-// Configuração do Knex (certifique-se de que ela corresponda à sua configuração real)
+
 const knexConfig = {
-  client: "sqlite3", // O cliente de banco de dados que você está usando (exemplo: PostgreSQL)
+  client: "sqlite3",
   connection: {
     filename: "data/portal.sqlite",
   },
-  useNullAsDefault: true, // Adiciona esta linha para evitar o aviso sobre valores padrão
+  useNullAsDefault: true,
 };
 
-// Carrega o conteúdo do arquivo SQL
 const sql = fs.readFileSync("dump.sql", "utf8");
 
 const db = knex(knexConfig);
@@ -18,9 +17,9 @@ const db = knex(knexConfig);
 db.raw(sql)
   .then(() => {
     console.log("Migração concluída com sucesso");
-    process.exit(0); // Encerra o processo Node.js com sucesso
+    process.exit(0); 
   })
   .catch((err) => {
     console.error("Erro durante a migração:", err);
-    process.exit(1); // Encerra o processo Node.js com erro
+    process.exit(1)
   });
